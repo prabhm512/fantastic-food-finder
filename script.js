7dee13a403640c6dfb5ab141c54ec6dc7f235643
Prabh Singh <prabhm512@gmail.com>
2020-09-22 22:59:41 +1000

    dynamically generating lat & lng

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 "use strict";
 
-var map;
+window.map = undefined;
 var service;
 
 // hard code location for initial testing
-var currLocation = { lat: -33.8665433, lng: 151.1956316 }; // pyrmont
+// var currLocation = { lat: -33.8665433, lng: 151.1956316 }; // pyrmont
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: currLocation,
-        zoom: 12,
-    });
-    service = new google.maps.places.PlacesService(map);
-    // when the map is set up do the call
-    getRestaurants();
+  const mapOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+
+  window.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
-function createMarker(place) {
-    // first create the image for the marker
-    var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-    };
+function moveToLocation(lat, lng) {
+  const center = new google.maps.LatLng(lat, lng);
+  window.map.panTo(center);
 
-    // then create the marker using that image
-    var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location,
-    });
+  service = new google.maps.places.PlacesService(map);
+  // when the map is set up do the call
+  getRestaurants();
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  moveToLocation(position.coords.latitude, position.coords.longitude);
+}
+
+getLocation();
+
+function createMarker(place) {
+  // first create the image for the marker
+  var image = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25),
+  };
+
+  // then create the marker using that image
+  var marker = new google.maps.Marker({
+    map: map,
+    icon: image,
+    title: place.name,
+    position: place.geometry.location,
+  });
 }
 
 function getRestaurants() {
-    var request = {
-        location: currLocation,
-        radius: "200",
-        query: "restaurant",
-    };
+  var request = {
+    location: window.map.center,
+    radius: "200",
+    query: "restaurant",
+  };
 
-    service.textSearch(request, callback);
+  service.textSearch(request, callback);
 
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            console.log(results);
-            for (var i = 0; i < results.length; i++) {
-                //var place = results[i];
-                createMarker(results[i]);
-            }
-        }
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results);
+      for (var i = 0; i < results.length; i++) {
+        //var place = results[i];
+        createMarker(results[i]);
+      }
     }
+  }
 }
